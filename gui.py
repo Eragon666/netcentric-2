2fc6c56e0a6214ed984f444985a682d254d6a97d
xanderkoning <xander.koning@student.uva.nl>
2014-06-11 11:14:01 +0200

    button werkt

--- gui.py
+++ gui.py
@@ -3,8 +3,7 @@ import sys
 
 from Tkinter import *
 
-def okdoei():
-	label2 = Label(root, text="OK DOEI", fg="#FF9231", bg="#666").pack()
+
 
 
 #create the window
@@ -14,12 +13,15 @@ root = Tk()
 root.title("Master control monitor")
 root.geometry("400x400+200+200")
 
+def okdoei():
+	label2 = Label(root, text="OK DOEI", fg="#FF9231", bg="#666").pack()
+
+bbut = Button(root, text='abi').grid(row=4)
 Label(root, text="plattegrond", fg="#FF9231", bg="#666").grid(row=0, columnspan=8)
 for r in range(3):
-    for c in range(4):
-        Label(root, text='R%s/C%s'%(r,c), fg="#FF9231", bg="#666", borderwidth=1).grid(row=r+1, column=c)
+	for c in range(4):
+		Label(root, text='R%s/C%s'%(r,c), fg="#FF9231", bg="#666", borderwidth=1).grid(row=r+1, column=c)
 
-#bluetoothButton = Button(root, text="Bluetooth", command=okdoei)
-#bluetoothButton.pack()
+bbut = Button(root, text='abi', command = okdoei).grid(row=4)
 #ONLY FOR WINDOWS USERS!! niet voor Daan haha
 root.mainloop()
