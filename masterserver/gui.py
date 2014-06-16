c69224adee2bcfbb4138afbacd144678643f277c
DDaaaaann <daandegraaf@wcw-stud-145-109-8-154.wireless.uva.nl>
2014-06-16 15:22:34 +0200

    Gui basic

--- masterserver/gui.py
+++ masterserver/gui.py
@@ -1,27 +1,58 @@
-#simple GUI
-import sys
 
+import Tkinter as tk
 from Tkinter import *
+import tkMessageBox
+
+canvas_width = 800
+canvas_height = 600
+
+def handler():
+	if tkMessageBox.askokcancel("Quit?", "Are you sure you want to quit?"):
+		root.quit()
+
+def connect():
+	print "connecting..."
 
 
 
 
-#create the window
-root = Tk()
 
-#modify root window
-root.title("Master control monitor")
-root.geometry("400x400+200+200")
 
-def okdoei():
-	label2 = Label(root, text="OK DOEI", fg="#FF9231", bg="#666").pack()
+class Gui():
+    def __init__(self, root):
+        self.root=root
+        self.entry = tk.Entry(root)
+        stvar=tk.StringVar()
+        stvar.set("one")
+        self.option=tk.OptionMenu(root, stvar, "one", "two", "three")
 
-bbut = Button(root, text='abi').grid(row=4)
-Label(root, text="plattegrond", fg="#FF9231", bg="#666").grid(row=0, columnspan=8)
-for r in range(3):
-	for c in range(4):
-		Label(root, text='R%s/C%s'%(r,c), fg="#FF9231", bg="#666", borderwidth=1).grid(row=r+1, column=c)
+        self.canvas=tk.Canvas(root, width=canvas_width, height=canvas_height, background='grey')
+        self.canvas.grid(row=0,column=1)
 
-bbut = Button(root, text='abi', command = okdoei).grid(row=4)
-#ONLY FOR WINDOWS USERS!! niet voor Daan haha
-root.mainloop()
+        frame = Frame(self.root)
+        frame.grid(row=0,column=0, sticky="n")
+        label1=Label(frame, text="Grid size").grid(row=0,column=0, sticky="nw")
+        label2=Label(frame, text="X").grid(row=1,column=0, sticky="w")
+        label3=Label(frame, text="Y").grid(row=2,column=0, sticky="w")
+        self.option.grid(row=0,column=1,sticky="nwe")
+        entryX = Entry(frame).grid(row = 1,column = 1,sticky = E+ W)
+        entryY = Entry(frame).grid(row = 2,column = 1, sticky = E)
+
+        Button1=Button(frame,text="Connect",command=connect).grid(row = 3,column = 1, sticky = "we")
+
+        for i in range(0,4):
+        	for j in range(0,4):	
+				figure1=self.canvas.create_rectangle(50*i, 50*j, 20+50*i, 20+50*j, fill="blue")
+        # Grid.columnconfigure(self.root,1,weight=1, size=200)
+
+if __name__== '__main__':
+    root=tk.Tk()
+    root.resizable(width=FALSE, height=FALSE)
+    gui=Gui(root)
+    root.protocol("WM_DELETE_WINDOW", handler)
+    root.mainloop()
+
+
+
+
+
