b510a239bdba84a052c5a3179344277538257450
xanderkoning <xander.koning@student.uva.nl>
2014-06-17 11:58:55 +0200

    werkende versie voor matthijs

--- bluetoothcommunication/app/src/main/java/com/example/bluetoothcommunication/app/BluetoothThread.java
+++ bluetoothcommunication/app/src/main/java/com/example/bluetoothcommunication/app/BluetoothThread.java
@@ -3,8 +3,11 @@ package com.example.bluetoothcommunication.app;
 import android.bluetooth.BluetoothSocket;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 /**
@@ -34,7 +37,7 @@ public class BluetoothThread extends Thread {
     public static BluetoothThread newInstance(BluetoothSocket socket, Listener listener) {
         BluetoothThread instance = new BluetoothThread(socket, listener);
         instance.start();
-        instance.run();
+        //instance.run();
         return instance;
     }
 
@@ -86,35 +89,43 @@ public class BluetoothThread extends Thread {
 
                 // check if reading is done
                 if (curLength > 0) {
-                    // reading finished
-                    listener.onReceived(buffer, curLength);
-                    curLength = bytes = 0;
-                }
-            }
-        } catch (IOException e) {
-            if (isClosing)
-                return;
-
-            listener.onError(e);
-            throw new RuntimeException(e);
-        }
+        listener.onDisconnected();
+        // reading finished
+        listener.onReceived(buffer, curLength);
+        curLength = bytes = 0;
     }
+}
+} catch (IOException e) {
+        if (isClosing)
+        return;
+
+        listener.onError(e);
+        throw new RuntimeException(e);
+        }
+        }
+
+public void read(byte[] bytes) throws IOException {
+    DataInputStream dis2 = new DataInputStream(socket.getInputStream());
+    InputStreamReader disR2 = new InputStreamReader(dis2);
+    BufferedReader br = new BufferedReader(disR2);//create a BufferReader object for input
+    //toastShort(br.toString());
+
+}
 
     /* Call this from the main Activity to send data to the remote device */
-    public void write(byte[] bytes) throws IOException {
+public void write(byte[] bytes) throws IOException {
         if (outputStream == null)
-            throw new IllegalStateException("Wait connection to be opened");
+        throw new IllegalStateException("Wait connection to be opened");
 
         outputStream.write(bytes);
         outputStream.flush();
-    }
+        }
 
 
-    /** Will cancel an in-progress connection, and close the socket */
-    public void cancel() throws IOException {
+/** Will cancel an in-progress connection, and close the socket */
+public void cancel() throws IOException {
         isClosing = true;
         socket.close();
-        listener.onDisconnected();
 
         inputStream = null;
         outputStream = null;
