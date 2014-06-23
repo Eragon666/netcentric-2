18ac8e2049c3387b3d3b6578970547bb8850813c
Eragon666 <matthijsthoolen@hotmail.com>
2014-06-23 11:27:44 +0200

    temp

--- RobotApp/app/src/main/java/uva/nc/app/MainActivity.java
+++ RobotApp/app/src/main/java/uva/nc/app/MainActivity.java
@@ -103,7 +103,7 @@ public class MainActivity extends ServiceActivity {
     private boolean previewing = true;
 
     // Initialize variables for bluetooth connection
-    private String deviceAddress = "00:09:DD:50:8D:2A";
+    private String deviceAddress = "00:15:83:15:A3:10";
     private UUID MY_UUID = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee");
 
     //Device address Matthijs: "00:15:83:15:A3:10";
