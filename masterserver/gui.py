52135afed335be5c46f294bfa44f262c71e8d727
Floris Turkenburg <sk8_floris@hotmail.com>
2014-06-24 11:38:16 +0200

    Gui aangepast

--- masterserver/gui.py
+++ masterserver/gui.py
@@ -23,6 +23,15 @@ robotY = 0
 global ID
 ID = 0
 
+
+global OccupiedLocations
+OccupiedLocations = [[0 for x in xrange(5)] for x in xrange(5)] 
+for i in range(5):
+        OccupiedLocations[4][i] = 1
+        OccupiedLocations[0][i] = 1
+        OccupiedLocations[i][4] = 1
+        OccupiedLocations[i][0] = 1
+        
 global robots
 robots = {}
 
@@ -68,13 +77,10 @@ advertise_service( server_sock, "SampleServer",
 def listenBluetooth():
     currentLocation = "[1, 1]"
     global direction
+    global OccupiedLocations
     confirmation = "False"
 
     ReservedLocations = ["[0, 1]", "[0, 2]", "[0, 3]", "[4, 1]", "[4, 2]", "[4, 3]", "[1, 0]", "[2, 0]", "[3, 0]", "[1, 4]", "[2, 4]", "[3, 4]"]
-    OccupiedLocations = [[0 for x in xrange(4)] for x in xrange(4)] 
-    for i in range(4):
-        OccupiedLocations[3][i] = 1
-        OccupiedLocations[i][3] = 1
     
     
     
@@ -113,7 +119,14 @@ def listenBluetooth():
         if newPosition in ReservedLocations:
             print "locations string"
             return "False"
-              
+        
+        
+        OccupiedLocations[newX][newY] = 1
+        OccupiedLocations[currentX][currentY] = 0
+        
+        for i in range(5):
+            print OccupiedLocations[i]
+            
         return "True"
     
     global root
