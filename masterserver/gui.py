cd2a7ca7e1fdd2073b0b73da786c291707daef3a
Eragon666 <matthijsthoolen@hotmail.com>
2014-06-24 14:20:55 +0200

    Code cleaned, working on grid

--- masterserver/gui.py
+++ masterserver/gui.py
@@ -183,40 +183,7 @@ def listenBluetooth():
                 else:
                     print "connection", addr, "closed"
                     s.close()
-                    read.remove(s)
-        
-        #print quit
-        """
-        if (quit):
-            for client in clients:
-                client[0].close()
-            break
-        elif (listen):
-            try:
-                for client in clients:
-                    data = client[0].recv(1024)
-                    if len(data) == 0:
-                        pass
-                    else:
-                        if "QRdata: " in data:
-                            QRdata = data.replace("QRdata: ", "")
-                            currentLocation = QRdata.replace("Locatie: ", "").replace("; Robot-ID: 21;", "")
-                            client[0].send("currentLocation: " + currentLocation)
-                        elif "direction: " in data:
-                            direction = data.replace("direction: ", "")
-                            confirmation = Confirm(currentLocation, direction)
-                            if confirmation =="True":
-                                client[0].send("confirmation: " + confirmation)
-                            else:
-                                client[0].send("currentLocation: " + currentLocation)
-                        print data
-                        parser(data)
-                        gui()
-                        print("received [%s]" % data)
-                
-            except IOError:
-                pass
-                """
+                    read.remove(s) 
 
 def guiMain():
     global root
@@ -279,20 +246,14 @@ def drawRobot(x,y,size,direction,xco,yco,canvas):
         figure = canvas.create_polygon(xleft, yup, xright, yup, xleft+size/4, ydown, fill="red")
     elif (direction == "West"):
         figure = canvas.create_polygon(xleft, ydown-size/4, xright, ydown, xright, yup, fill="red")
-    #figure=canvas.create_rectangle(x+size/2,canvas_height-y -size/4,x+size,canvas_height-y-size+size/4, fill="yellow")
-
-#def parser(self, data):
-    #for s in data:
-        #if s
-     
-    #self.canvas.update_idletasks()
-    #drawRobot(self,20,20,300,1,1,1)
 
 def gui():
     global root
     global direction
     global x
     global y
+    global OccupiedLocations
+
     entry = tk.Entry(root)
     stvar=tk.StringVar()
     stvar.set("one")
@@ -300,8 +261,10 @@ def gui():
     canvas=tk.Canvas(root, width=canvas_width, height=canvas_height, background='grey')
     canvas.grid(row=0,column=1,ipadx=10,ipady=10)
 
-    #frame = Frame(self.root)
-    #frame.grid(row=0,column=0, sticky="n")
+    if not OccupiedLocations[x][y]:
+            #OccupiedLocations[x][y][1] = [False, 1, 0]
+            print("Added shit to grid")
+
     label1=Label(root, text="Grid size")
     label1.grid(row=0,column=0, sticky="nw")
     label2=Label(root, text="X").grid(row=1,column=0, sticky="w")
@@ -309,7 +272,6 @@ def gui():
     e = Entry(root)
     e.grid(row = 1,column = 1,sticky = E+ W)
     text = e.get()
-    print text
     Button1=Button(root,text="Connect",command=connect).grid(row = 3,column = 1, sticky = "we")
 
     for i in range(x):
@@ -318,13 +280,8 @@ def gui():
             dy = canvas_height/y
             drawQR(dx*i,dy*j,dx/2, canvas)
 
-    for keys,values in robots.items():
-        print(keys)
-        print(values)
     for key, value in robots.items():
         drawRobot(dx,dy,dx/2,value[3],value[1],value[2],canvas)
-    #drawRobot(dx,dy,300,1,0,0,canvas)
-    #root.mainloop()
 
 def handler(self):
     global quit
