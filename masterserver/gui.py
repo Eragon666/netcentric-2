7f15a6946ad9a1132dc2a9b8bba9de7a0f61d70d
xanderkoning <xander.koning@student.uva.nl>
2014-06-24 15:32:57 +0200

    -tje veranderd

--- masterserver/gui.py
+++ masterserver/gui.py
@@ -256,7 +256,7 @@ def drawQR(canvas):
     for i in range(x):
         for j in range(y):
             if (blocks[(i,j)][0]):
-                figure = canvas.create_rectangle(dx*i, canvas_height-dy*j, dx*i+size, canvas_height-dy*j+size, fill="white")
+                figure = canvas.create_rectangle(dx*i, canvas_height-dy*j, dx*i+size, canvas_height-dy*j-size, fill="white")
         
 def drawRobot(direction,xco,yco,canvas):
     xco -= 1
